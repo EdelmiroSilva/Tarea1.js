eaeb8e6ca3fa99c2c512704dcf4bd227e7102bc2
EdelmiroSilva <42301974+EdelmiroSilva@users.noreply.github.com>
2019-02-01 19:40:04 -0600

    Create figura2.js

---
+++ figura2.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+ fill(255,0,0);
+	rect(0,0,133,400);
+	fill(0,255,0);
+	rect(133,0,266,400);
+	fill(0,0,255);
+	rect(266,0,400,400);
+
+	 
+}
